07f0ace510db2a1dbe1ae660bf638d4d01f3f2a3
Andrew <andcher53@gmail.com>
2023-12-23 19:18:40 +0200

    rename bad names

---
+++ day3/part-1.mjs
@@ -0,0 +1,12 @@
+import { open } from 'fs/promises';
+
+const file = await open('sinput.txt');
+const contents = await file.readFile({ encoding: "utf-8" });
+const lines = contents.split('\r\n');
+
+let finalResult = 0;
+
+console.log(lines);
+
+
+
